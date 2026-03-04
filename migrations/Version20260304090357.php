97b1092f560ff6b06cf9dfb4a26716cf6cfe59cf
Nico13M <martinez.nicolas13320@gmail.com>
2026-03-04 10:09:34 +0100

    FEAT: Ajout de la relation ingredient et diet

---
+++ migrations/Version20260304090357.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260304090357 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingredient ADD units_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE ingredient ADD CONSTRAINT FK_6BAF787099387CE8 FOREIGN KEY (units_id) REFERENCES units (id) NOT DEFERRABLE');
+        $this->addSql('CREATE INDEX IDX_6BAF787099387CE8 ON ingredient (units_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA pgbouncer');
+        $this->addSql('CREATE SCHEMA realtime');
+        $this->addSql('CREATE SCHEMA extensions');
+        $this->addSql('CREATE SCHEMA vault');
+        $this->addSql('CREATE SCHEMA graphql_public');
+        $this->addSql('CREATE SCHEMA graphql');
+        $this->addSql('CREATE SCHEMA auth');
+        $this->addSql('CREATE SCHEMA storage');
+        $this->addSql('ALTER TABLE ingredient DROP CONSTRAINT FK_6BAF787099387CE8');
+        $this->addSql('DROP INDEX IDX_6BAF787099387CE8');
+        $this->addSql('ALTER TABLE ingredient DROP units_id');
+    }
+}

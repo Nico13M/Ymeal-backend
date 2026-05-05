024b4f5ca3f148ec346117a84ca9b0bb0ce658db
Nico13M <martinez.nicolas13320@gmail.com>
2026-05-05 14:18:45 +0200

    fix

--- src/Controller/Admin/AdminFrigoController.php
+++ src/Controller/Admin/AdminFrigoController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 use App\Entity\Frigo;
 use App\Entity\FrigoIngredient;
 use App\Entity\Ingredient;
-use App\Entity\Units;
 use App\Entity\User;
 use App\Repository\IngredientRepository;
 use App\Repository\UnitsRepository;

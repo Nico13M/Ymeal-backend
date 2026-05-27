5cd0c505abc93cf6f664c89d623ce972ff16d9e9
Nico13M <martinez.nicolas13320@gmail.com>
2026-05-27 16:12:30 +0200

    fix

--- src/Controller/Admin/AdminRecipeController.php
+++ src/Controller/Admin/AdminRecipeController.php
@@ -87,8 +87,6 @@ class AdminRecipeController extends AbstractController
 
         // Appeler le service de recherche
         $result = $this->recipeSearchService->searchRecipes($user, $request);
-dump($result);
-dd($result['data']);
         return $this->jsonWithUserId($result['data'], 200);
     }
 

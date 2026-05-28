5b8a7a6a940488d6a1d6b62ff8a6f29835ce807e
Nico13m <martinez.nicolas13320@gmail.com>
2026-05-28 04:34:27 +0200

    FIX: Import

--- src/Controller/Admin/AdminReferenceController.php
+++ src/Controller/Admin/AdminReferenceController.php
@@ -7,6 +7,7 @@ use App\Repository\DietRepository;
 use App\Repository\FavoriteCuisineRepository;
 use App\Repository\IngredientRepository;
 use App\Entity\User;
+use App\Service\UserManager; 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
